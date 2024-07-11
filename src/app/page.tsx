b2028cbb8ceddbeeb0a75132e6cad6a00f24564f
KaterynaMishchuk12 <138882300+KaterynaMishchuk12@users.noreply.github.com>
2024-07-11 10:29:46 +0300

    practiced more on client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,14 @@
-import course from "./course.json";
-import { Lesson } from "./lesson";
-import styles from "./page.module.css";
+import dynamic from "next/dynamic";
 
-async function getAllCourses(): Promise<typeof course> {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(course), 3000);
-  });
-}
+const ClientComponent = dynamic(() => import("./some-component"), {
+  ssr: false,
+});
 
 export default async function Home() {
-  const courses = await getAllCourses();
   return (
-    <main className={styles.main}>
-      <ul>
-        {courses.lessons.map((lesson) => {
-          return (
-            <li key={lesson.name}>
-              <Lesson title={lesson.title} points={lesson.name} />
-            </li>
-          );
-        })}
-      </ul>
+    <main>
+      <h2>React</h2>
+      <ClientComponent />
     </main>
   );
 }
---
+++ src/app/some-component.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+export default function SomeComponent() {
+  console.log("SOME COMPONET RENDERED");
+  return <p>SOME SUPER COMPONENT IS HERE</p>;
+}
